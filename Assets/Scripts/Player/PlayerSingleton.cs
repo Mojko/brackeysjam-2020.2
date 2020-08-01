ecc29e36e43cb4d8c4213ac73e1c831acafb01fc
Mojkoo <mojkoow@gmail.com>
2020-08-02 00:00:35 +0200

    added items

---
+++ Assets/Scripts/Player/PlayerSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+    private static PlayerSingleton instance = new PlayerSingleton();
+    public static PlayerSingleton Instance => instance;
+
+    public delegate void ItemChangedEvent(ItemData to);
+    public static event ItemChangedEvent OnItemChanged;
+
+    private ItemData currentEquippedItem;
+    public ItemData CurrentEquippedItem
+    {
+        get { return currentEquippedItem;  }
+        set { OnItemChanged.Invoke(value); currentEquippedItem = value;  }
+    }
+}
